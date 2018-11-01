910de62bc23f411057b59571d3770efedd4162d2
Joel Fecke <jfecke@gmail.com>
2018-10-31 21:26:55 -0500

    added gif on click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,13 +8,17 @@ var gify = {
             newElm.on("click", function() {
                 $("#gifarea").empty();
                 var content = this.textContent;
-                var GIFobj = $.get("https://api.giphy.com/v1/gifs/search?q=" + content  +"&api_key=C4vD2rTzNQERymftt8uTanePEkBN0ZEd&limit=5");
+                var GIFobj = $.get("http://api.giphy.com/v1/gifs/search?q=" + content  +"&api_key=C4vD2rTzNQERymftt8uTanePEkBN0ZEd&limit=5");
                 GIFobj.done(function(data) { 
                     for (var j=0; j <= data.data.length-1; j++) {
-                        console.log(j);
-                        console.log(data[j]);
                         var newGIF = $("<img>");
-                        newGIF.attr("src", data.data[j].images.fixed_height.url)
+                        newGIF.attr("src", data.data[j].images.fixed_width_still.url)
+                        newGIF.attr("alt", data.data[j].images.fixed_width.url) 
+                        newGIF.on("click", function(){
+                            var swap = this.src;
+                            this.src = this.alt;
+                            this.alt = swap;
+                        })
                         $("#gifarea").append(newGIF)
                      }
                 })
