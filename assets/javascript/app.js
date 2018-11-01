92bf3514b3857fd6ccf503a436ea974e432952b6
Joel Fecke <jfecke@gmail.com>
2018-10-31 20:38:23 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var gify = {
+    topics: ["tigers","gorillas","hawks"],
+    populateArea: function(myArray) {
+        for (var i = 0; i <= myArray.length-1; i++) {
+            var newElm = $("<div>");
+            newElm.text(myArray[i]);
+            newElm.addClass("tags");
+            newElm.on("click", function() {
+                $("#gifarea").empty();
+                var content = this.textContent;
+                var GIFobj = $.get("http://api.giphy.com/v1/gifs/search?q=" + content  +"&api_key=C4vD2rTzNQERymftt8uTanePEkBN0ZEd&limit=5");
+                GIFobj.done(function(data) { 
+                    for (var j=0; j <= data.data.length-1; j++) {
+                        console.log(j);
+                        console.log(data[j]);
+                        var newGIF = $("<img>");
+                        newGIF.attr("src", data.data[j].images.fixed_height.url)
+                        $("#gifarea").append(newGIF)
+                     }
+                })
+            })
+            $("#tagarea").append(newElm);
+        }
+    }   
+}
+
+gify.populateArea(gify.topics);
+
+$("#submit").on("click", function(){
+    if ($("#giftext").val().length > 0) {
+        gify.populateArea([$("#giftext").val()]);
+        $("#giftext").val("");
+    }
+    
+    
+})
+
